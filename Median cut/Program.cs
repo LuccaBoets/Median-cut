6b420ad973d45dc303c94ec1be5c1b5b3db18e14
Lucca Boets <boetslucca@gmail.com>
2020-11-15 18:30:16 +0100

    init

---
+++ Median cut/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Median_cut
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
